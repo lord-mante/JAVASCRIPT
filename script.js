81370084063b184e691cb238050ca2000e886315
lord-mante <mantelord01@gmail.com>
2025-10-01 12:42:53 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Strings 
+let name = "John";
+let box =  "suguar";
+let cars = "Nissan";
+let teams = "Barca";
+let boxes = "laptops";
+
+// numbers 
+let a = 4;
+let b = 78;
+let c = 0.3;
+
+// boolean 
+let isStudent = true;
+let hasCoupon = false;
+
+
+
+
+// let box = "laptop"
+
+//  let a = 4 
+//  let b = 5
+//  console.log(++a, --b)
+
+//  let a = 4 
+//  let b = 7
+//  console.log(a == b)
+
+// let age = 16;
+// let hasReferralCode = true;
+// let isStudent = true;
+
+// if((age === 18 && hasReferralCode) || isStudent){
+//     console.log("Eligible for gym membership!")
+// }
+// else{
+//     console.log ("Not eligible for gym membership!")
+// }
+
+// let premuimMember = true;
+// let totalPrice = 60;
+// let hasCoupon = true;
+
+// if ((totalPrice >= 50 && hasCoupon) || premuimMember){
+//     console.log("Can get free shipping!")
+// }
+// else{
+//     console.log("No free shipping!")
+// }
+
+// let userName = "Harrison";
+// let defualtName = "Guest"
+
+// let displayedName = userName || defualtName
+
+// console.log(displayedName)
